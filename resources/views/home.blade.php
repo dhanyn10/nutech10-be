61163af65cef8c5f5872cc38a3f5dd4d111d41d5
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-12 19:34:43 +0700

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('content')
+    <div class="card">
+        <div class="card-header">items</div>
+        <div class="card-body">
+            hello
+        </div>
+    </div>
+@endsection
