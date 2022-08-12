6279436f06690a48251bca2d9645c42939f90cf5
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-12 22:03:47 +0700

    update link navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+        <a class="navbar-brand" href="{{route('home')}}">{{config('app.name')}}</a>
     </div>
 </nav>
