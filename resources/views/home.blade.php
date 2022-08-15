d7fe3caed349c3c343554d86a295f4604acbb0f5
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-15 18:40:40 +0700

    update src

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,8 @@
             <div class="col-md-4">
                 <div class="card">
                     <?php $url = env('API_URL').$port.DIRECTORY_SEPARATOR.$item['foto'] ?>
-                    <img src="{{DirSeparator::dir($url)}}" alt="">
+                    {{-- <img src="{{DirSeparator::dir($url)}}" alt=""> --}}
+                    <img src="{{str_replace("\\", "/",$url)}}" alt="">
                     <div class="card-body">
                         <div class="card-title">{{$item['nama']}}</div>
                         <div class="d-grid">
