018e042a9f04d738ca44f333ff99f6f51c93080f
dhanyn10 <dhanyn.dhan@gmail.com>
2022-08-15 18:27:01 +0700

    apply helper

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-4">
                 <div class="card">
                     <?php $url = env('API_URL').$port.DIRECTORY_SEPARATOR.$item['foto'] ?>
-                    <x-image dir="{{$url}}" alt=""/>
+                    <img src="{{DirSeparator::dir($url)}}" alt="">
                     <div class="card-body">
                         <div class="card-title">{{$item['nama']}}</div>
                         <div class="d-grid">
